a00d6a7b76a2e4f15b38b52ef9b1431e87a6385a
Bron200089 <200089@virtualwindow.co.za>
2022-08-01 00:16:48 +0200

    Landing page almost complete

--- client/src/Pages/LandingPage.js
+++ client/src/Pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProductSlider from '../components/ProductSlider';
+import ProductCard from '../components/ProductCard';
 
 function LandingPage () {
 
@@ -28,6 +29,25 @@ function LandingPage () {
                 non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                 <ProductSlider/>
+            </div>{/* new to store */}
+
+            {/* new to store */}
+            <div className="saleItemContainer">
+                <h3>Now</h3>
+                <h1>On Sale!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br/>
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation<br/>
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit<br/> 
+                </p>
+
+                <div className='sales'>
+                    <div className='saleInfo'>
+                        <h2>Buy Two,<br/>Get One Free!</h2>
+                        <p>We have an amazing deal for you guys this August.<br/> If you buy any two small plants, you get one free!</p>
+                        <div className='saleImage'></div>
+                        <button>Shop Now!</button>
+                    </div>
+                </div>
             </div>{/* new to store */}
         </div>
     )
