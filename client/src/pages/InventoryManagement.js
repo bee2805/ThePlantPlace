12fc5d8763e825f2db6386d2ccce43c0dfe0c3d8
Bron200089 <200089@virtualwindow.co.za>
2022-08-05 11:17:37 +0200

    Inventory Admin

--- client/src/pages/InventoryManagement.js
+++ client/src/pages/InventoryManagement.js
@@ -3,6 +3,7 @@ import ProductCard from '../components/ProductCard';
 
 function InventoryManagement () {
     return(
+        <>
         <div className="InventoryManagement">
             <h1>Manaage Inventory</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br/>
@@ -11,14 +12,25 @@ function InventoryManagement () {
             in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat <br/>
             non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-            <div className='addItem'>
-                <h2>+ Add a new Product</h2>
+            <div className='products'>
+                <a><div className='addItem'>
+                    <h1>+ Add <br/>new product</h1>
+                </div></a>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
             </div>
-
-            <ProductCard/>
-            
             
         </div>
+        {/* footer */}
+        <div className='footer'>
+                <p>ThePlantPlace©2022</p>
+                <a href="tel:123-456-7890"><div className='call'></div></a>
+                <a href="mailto: ThePlantPlace@gmail.com"><div className='mail'></div></a>
+                <a href="https://instagram.com"><div className='instagram'></div></a>
+            </div>
+        </>
     )
 }
 
