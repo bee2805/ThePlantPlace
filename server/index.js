8e229b7477d53eb90befc37392cc49cfe80f1890
200089_Bee! <200089@virtualwindow.co.za>
2022-11-10 10:30:02 +0200

    small changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 require('dotenv/config');
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3002'
 }));
 
 app.use('/productImages', express.static("productImages"));
